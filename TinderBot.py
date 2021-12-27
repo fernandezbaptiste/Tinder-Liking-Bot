fde91d8c195467030e1924884c5497d3562c059e
Baptiste-Fernandez <fernandez.baptiste1@gmail.com>
2021-12-27 23:59:01 +0100

    Bot changes

--- TinderBot.py
+++ TinderBot.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.options import Options
 from random import randint
 import json
 
+# Launch Chrome on Tinder's website
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://www.tinder.com')
 print("Opened tinder")
@@ -18,18 +19,18 @@ with open('creds.json') as json_file:
 email = data['email']
 password = data['pass']
 
+# Handle Poped Up Window
 main_page = driver.current_window_handle
 sleep(1)
-
 print(main_page)
 
-cookies = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div/div[1]/button")
-cookies.click()
+# Deal with Cookies
+cookies = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div/div[1]/button").click()
 print("Clicked Accepted")
 sleep(2)
 
-login_first_page = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div[3]/span/div[1]/div/button")
-login_first_page.click()
+# Deal with Login Page
+login_first_page = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div[3]/span/div[1]/div/button").click()
 print("Clicked Login")
 sleep(1)
 
@@ -45,20 +46,16 @@ sleep(1)
 driver.switch_to.window(login_page)
 sleep(1)
 
-email = driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input")
-email.send_keys("baptistefernandez2008@gmail.com")
+email = driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input").send_keys(email)
 sleep(1)
 
-email_accept = driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/span/span")
-email_accept.click()
+email_accept = driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/span/span").click()
 sleep(1)
 
-password = driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input")
-password.send_keys(password)
+password = driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input").send_keys(password)
 sleep(1)
 
-password_accept = driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/span/span")
-password_accept.click()
+password_accept = driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/span/span").click()
 sleep(1)
 
 driver.switch_to.window(main_page)
@@ -66,30 +63,25 @@ sleep(5)
 print(driver.window_handles)
 
 
-allow_location = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div[3]/button[1]/span")
-allow_location.click()
+allow_location = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div[3]/button[1]/span").click()
 print("Allowed Notifications")
 sleep(1)
 
-enable_notifications = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div[3]/button[1]")
-enable_notifications.click()
+enable_notifications = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div[3]/button[1]").click()
 print("Enabled Notifications")
 
 sleep(3)
 
-message_button = driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/aside/nav/div/div/div/div[1]/div/div[2]/button")
-message_button.click()
+message_button = driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/aside/nav/div/div/div/div[1]/div/div[2]/button").click()
 print("Liked")
 sleep(2)
 
 def like():
-    like_button = driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button")
-    like_button.click()
+    like_button = driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button").click()
     sleep(1)
 
 def dislike():
-    dislike = driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[2]/button")
-    dislike.click()
+    dislike = driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[2]/button").click()
     sleep(1)
 
 def random_number():
@@ -112,10 +104,8 @@ while True:
     except Exception:
         try:
             match_go_back_main_menu = driver.find_element_by_xpath(
-                "/html/body/div[1]/div/div[1]/div/main/div[2]/div/div/div[1]/div/div[3]/a")
-            match_go_back_main_menu.click()
+                "/html/body/div[1]/div/div[1]/div/main/div[2]/div/div/div[1]/div/div[3]/a").click()
             sleep(1)
         except Exception:
-            close_popup = driver.find_element_by_xpath("/html/body/div[2]/div/div/div[2]/button[2]")
-            close_popup.click()
+            close_popup = driver.find_element_by_xpath("/html/body/div[2]/div/div/div[2]/button[2]").click()
             sleep(1)
